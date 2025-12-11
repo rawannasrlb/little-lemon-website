7bca4a371e2354b8f23af7aa8235f4b425cc3410
rawannasrlb <rawan.nasr.lb@outlook.com>
2025-12-11 13:12:23 +0200

    replace social links with icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -17,7 +17,7 @@ const Footer = () => {
         {/* Navigation links */}
         <div className="order-1 lg:order-2 flex flex-col text-start gap-5 ">
           <h3 className="text-primary-dark">Navigation</h3>
-          <ul className="footer-links flex flex-col gap-2 lg:gap-4">
+          <ul className="footer-links flex flex-col gap-2 lg:gap-4 text-[#686868]">
             <li>
               <HashLink smooth to="/#about">
                 About
@@ -44,32 +44,65 @@ const Footer = () => {
           </ul>
         </div>
         {/* Contact links */}
-        <div className="order-2 lg:order-3 flex flex-col text-start gap-5">
+        <div className="order-3 lg:order-3 col-span-2 lg:col-span-1 flex flex-col text-start gap-5">
           <h3 className="text-primary-dark">Contact</h3>
           <ul className="footer-links flex flex-col gap-2 lg:gap-4">
-            <li>
+            <li className="flex items-center gap-3 text-[#686868]">
+              <i className="fa-solid fa-location-dot fa-sm text-primary-dark"></i>
               <a href="/">Location</a>
             </li>
-            <li>
-              <a href="/">Phone Number</a>
+            <li className="flex items-center gap-3 text-[#686868]">
+              <i className="fa-solid fa-phone fa-sm text-primary-dark"></i>
+              <a href="/">+1 00000 000 0</a>
             </li>
-            <li>
-              <a href="/">Email Address</a>
+            <li className="flex items-center gap-3 text-[#686868]">
+              <i className="fa-solid fa-envelope fa-sm text-primary-dark"></i>
+              <a href="/">support@littlelemon</a>
             </li>
           </ul>
         </div>
         {/* Social links */}
-        <div className="order-3 lg:order-4 col-span-2 lg:col-span-1 flex flex-col text-start gap-5">
+        <div className="order-2 lg:order-4 flex flex-col text-start gap-5">
           <h3 className="text-primary-dark">Socials</h3>
-          <ul className="footer-links flex flex-col gap-2 lg:gap-4">
+          <ul className="footer-links flex gap-3 lg:gap-5">
             <li>
-              <a href="/">Facebook</a>
+              <a
+                href="https://www.facebook.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-facebook-f fa-md text-[#686868] hover:text-highlight-light"></i>
+              </a>
             </li>
+
             <li>
-              <a href="/">Twitter</a>
+              <a
+                href="https://www.instagram.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-instagram fa-md text-[#686868] hover:text-highlight-light"></i>
+              </a>
             </li>
+
             <li>
-              <a href="/">Instagram</a>
+              <a
+                href="https://www.twitter.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-x-twitter fa-md text-[#686868] hover:text-highlight-light"></i>
+              </a>
+            </li>
+
+            <li>
+              <a
+                href="https://www.tiktok.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fab fa-tiktok fa-md text-[#686868] hover:text-highlight-light"></i>
+              </a>
             </li>
           </ul>
         </div>
